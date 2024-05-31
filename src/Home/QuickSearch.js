d729289cb7f878c7bf35fae4bf0386f980083a3b
ashish khose <100353863+Ashishkhose7@users.noreply.github.com>
2024-05-31 13:28:39 +0530

    Update QuickSearch.js

--- src/Home/QuickSearch.js
+++ src/Home/QuickSearch.js
@@ -18,7 +18,7 @@ class QuickSearch extends Component {
         <div className="card-container d-flex">
           <SectionHeadings title={"Discover Restaurants by Meal"} />
           <MealItemCards mealData={this.state.mealData} />
-          <SectionHeadings title={"Order Your Favourite Food Here"} />
+          <SectionHeadings title={"Order Your Favourite Food Here"} isfav={true}/>
           <FoodCards favfoodData={this.state.favfoodData} />
         </div>
       </div>
