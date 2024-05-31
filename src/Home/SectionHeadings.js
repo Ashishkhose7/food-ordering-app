46d8f5dd0df2d37088edee299073d94cd4a3092a
ashish khose <100353863+Ashishkhose7@users.noreply.github.com>
2024-05-31 13:18:29 +0530

    heading fix

--- src/Home/SectionHeadings.js
+++ src/Home/SectionHeadings.js
@@ -1,6 +1,6 @@
 const SectionHeadings = ({ title }) => {
   return (
-    <section className="quick-search-section-heading mx-4" id="dashboard">
+    <section className="quick-search-section-heading mx-2" id="dashboard">
       <h4 className="title-one-head">
         <span className="">{title}</span>
       </h4>
